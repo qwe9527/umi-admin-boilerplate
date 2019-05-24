0c86517ccd92a881b633d4e8e28102556a1c07ce
chent <chentaocoder@qq.com>
2019-05-24 17:40:56 +0800

    fixedbug

--- .umirc.js
+++ .umirc.js
@@ -39,28 +39,31 @@ export default {
       "to": "./"
     }
   ],
-  // chainWebpack(config) {
-  //   config.merge({
-  //     plugin: {
-  //       install: {
-  //         plugin: require('uglifyjs-webpack-plugin'),
-  //         args: [{
-  //           sourceMap: false,
-  //           uglifyOptions: {
-  //             compress: {
-  //               // 删除所有的 `console` 语句
-  //               drop_console: true,
-  //             },
-  //             output: {
-  //               // 最紧凑的输出
-  //               beautify: false,
-  //               // 删除所有的注释
-  //               comments: false,
-  //             },
-  //           }
-  //         }]
-  //       }
-  //     }
-  //   })
-  // },
+  chainWebpack(config) {
+    config
+      .when(process.env.NODE_ENV === 'production', cfg => {
+        cfg.merge({
+          plugin: {
+            install: {
+              plugin: require('uglifyjs-webpack-plugin'),
+              args: [{
+                sourceMap: false,
+                uglifyOptions: {
+                  compress: {
+                    // 删除所有的 `console` 语句
+                    drop_console: true,
+                  },
+                  output: {
+                    // 最紧凑的输出
+                    beautify: false,
+                    // 删除所有的注释
+                    comments: false,
+                  },
+                }
+              }]
+            }
+          }
+        })
+      });
+  },
 }
